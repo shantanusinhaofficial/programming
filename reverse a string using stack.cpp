dce7e8358ef926192c7e7cdfc7d1d2fe85437664
shantanusinha <107639795+shantanusinhaofficial@users.noreply.github.com>
2023-02-09 11:20:01 +0530

    Create reverse a string using stack.cpp

---
+++ reverse a string using stack.cpp
@@ -0,0 +1,23 @@
+// reverse a string using stack
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str = "shantanu";
+    stack<char> s;
+    for (int i = 0; i < str.length(); i++)
+    {
+        char ch = str[i];
+        s.push(ch);
+    }
+    string ans = "";
+    while (!s.empty())
+    {
+
+        char ch = s.top();
+        ans.push_back(ch);
+        s.pop();
+    }
+    cout << "answer is" << ans << endl;
+    return 0;
+}
