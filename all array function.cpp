8996fc1e09dad8511b70447a6fab9068470d7879
shantanusinha <107639795+shantanusinhaofficial@users.noreply.github.com>
2023-05-26 10:24:02 +0530

    Create all array function.cpp

---
+++ all array function.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+
+    for(int i=0; i<5; i++)
+    {
+        int element;
+        cin >> element;
+        v.push_back(element);
+    }
+
+    for(int i=0; i<v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    for(int ele : v)
+    {
+        cout << ele << " ";
+    }
+    cout << endl;
+
+    v.erase(v.end()-2);
+
+    for(int i=0; i<v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    v.insert(v.begin()+2, 6);
+
+    for(int i=0; i<v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
