3a0d8232d80f9ef85c80e5a412ac3601f5ef6252
shantanusinha <107639795+shantanusinhaofficial@users.noreply.github.com>
2023-05-27 12:52:40 +0530

    Update minimum numbers of balls in 1 boxes leetcode.cpp

--- minimum numbers of balls in 1 boxes leetcode.cpp
+++ minimum numbers of balls in 1 boxes leetcode.cpp
@@ -1,11 +1,21 @@
-class Solution:
-    def minOperations(self, boxes: str) -> List[int]:
-        dp=[0]*len(boxes)
-        for i in range(len(boxes)):
-            #left
-            for j in range(i+1):
-               if boxes[j]=="1" : dp[i]+=abs(i-j)
-               #right
-            for j in range(i,len(boxes)):
-               if boxes[j]=="1" : dp[i]+=abs(i-j)
-        return dp
+class Solution {
+public:
+    std::vector<int> minOperations(std::string boxes) {
+        std::vector<int> dp(boxes.size(), 0);
+        for (int i = 0; i < boxes.size(); i++) {
+            // Left
+            for (int j = 0; j <= i; j++) {
+                if (boxes[j] == '1') {
+                    dp[i] += std::abs(i - j);
+                }
+            }
+            // Right
+            for (int j = i; j < boxes.size(); j++) {
+                if (boxes[j] == '1') {
+                    dp[i] += std::abs(i - j);
+                }
+            }
+        }
+        return dp;
+    }
+};
