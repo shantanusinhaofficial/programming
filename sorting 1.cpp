1ad831f2efe09c97ffda95e9be1b57d7846dea1d
shantanusinha <107639795+shantanusinhaofficial@users.noreply.github.com>
2023-09-15 16:22:04 +0530

    Update sorting 1.cpp
    selection and bubble sort adder with their driver code in cpp

--- sorting 1.cpp
+++ sorting 1.cpp
@@ -27,3 +27,36 @@ int main()
     }
     return 0;
 }
+void bubble_Sort(int arr[], int n) 
+{
+    //write your code here
+    for(int i=n-1;i>=0;i--){
+        int didswap=0;
+        for(int j=0;j<n-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j] = temp;
+                didswap = 1;
+            }
+        }
+        if(didswap == 0){
+            break;
+        }
+        cout<<"runs\n";
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+      cin>>arr[i];
+    }
+   bubble_Sort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
